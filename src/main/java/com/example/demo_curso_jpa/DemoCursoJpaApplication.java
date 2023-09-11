4bbd4ff27edfd308eb408f35f6519b4614ec265f
Alisson <arteaga10alisson@gmail.com>
2023-09-11 16:21:51 -0500

    add kafka

--- src/main/java/com/example/demo_curso_jpa/DemoCursoJpaApplication.java
+++ src/main/java/com/example/demo_curso_jpa/DemoCursoJpaApplication.java
@@ -2,6 +2,7 @@ package com.example.demo_curso_jpa;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.annotation.EnableKafka;
 
 @SpringBootApplication
 public class DemoCursoJpaApplication {
